3ba9f58eb2f20346ac8615047a348a5c3e506dea
Dieter Plaetinck <dieter@raintank.io>
2015-09-08 13:24:35 +0300

    endpoint stuff

--- endpoint.go
+++ endpoint.go
@@ -10,6 +10,38 @@ import (
 	m "github.com/grafana/grafana/pkg/models"
 )
 
+type Endpoint struct {
+	Id    int64
+	OrgId int64 `json:"org_id"`
+	Name  string
+	Slug  string
+	Tags  []string
+}
+
+// note: grafana will override the org-id in your query with the active org based on your login.
+func (c *Client) Endpoints(settings m.GetEndpointsQuery) ([]Endpoint, error) {
+	data, _ := json.Marshal(settings)
+	endpoints := make([]Endpoint, 0)
+
+	req, err := c.newRequest("GET", "/api/endpoints/", bytes.NewBuffer(data))
+	if err != nil {
+		return endpoints, err
+	}
+	resp, err := c.Do(req)
+	if err != nil {
+		return endpoints, err
+	}
+	if resp.StatusCode != 200 {
+		return endpoints, errors.New(resp.Status)
+	}
+	data, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return endpoints, err
+	}
+	err = json.Unmarshal(data, &endpoints)
+	return endpoints, err
+}
+
 func (c *Client) NewEndpoint(settings m.AddEndpointCommand) error {
 	data, err := json.Marshal(settings)
 	req, err := c.newRequest("PUT", "/api/endpoints", bytes.NewBuffer(data))
@@ -25,6 +57,21 @@ func (c *Client) NewEndpoint(settings m.AddEndpointCommand) error {
 		return err
 	}
 	fmt.Println("response BODY", string(data))
+	if resp.StatusCode != 200 {
+		return errors.New(resp.Status)
+	}
+	return err
+}
+
+func (c *Client) DeleteEndpoint(id int64) error {
+	req, err := c.newRequest("DELETE", fmt.Sprintf("/api/endpoints/%d", id), nil)
+	if err != nil {
+		return err
+	}
+	resp, err := c.Do(req)
+	if err != nil {
+		return err
+	}
 	if resp.StatusCode != 200 {
 		return errors.New(resp.Status)
 	}
