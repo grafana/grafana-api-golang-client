a55e6879c82b1f5b2e54de798630d11b1b32eb49
Malcolm Jones <malcolm.jones@nytimes.com>
2018-06-27 18:06:42 -0400

    Add tests for orgs

--- grafana_fixtures_test.go
+++ grafana_fixtures_test.go
@@ -2,4 +2,11 @@ package gapi
 
 const (
 	createdDataSourceJSON = `{"id":1,"message":"Datasource added", "name": "test_datasource"}`
+
+	getOrgsJSON = `[{"id":1,"name":"Main Org."},{"id":2,"name":"Test Org."}]`
+	getOrgJSON = `{"id":1,"name":"Main Org.","address":{"address1":"","address2":"","city":"","zipCode":"","state":"","country":""}}`
+	createdOrgJSON = `{"message":"Organization created","orgId":1}`
+	updatedOrgJSON = `{"message":"Organization updated"}`
+	deletedOrgJSON = `{"message":"Organization deleted"}`
+
 )
---
+++ orgs_test.go
@@ -0,0 +1,99 @@
+package gapi
+
+import (
+	// "net/http"
+	// "net/http/httptest"
+	// "net/url"
+	"testing"
+
+	"github.com/gobs/pretty"
+)
+
+func TestOrgs(t *testing.T) {
+	server, client := gapiTestTools(200, getOrgsJSON)
+	defer server.Close()
+
+	orgs, err := client.Orgs()
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Log(pretty.PrettyFormat(orgs))
+
+	if len(orgs) != 2 {
+		t.Error("Length of returned orgs should be 2")
+	}
+	if orgs[0].Id != 1 || orgs[0].Name != "Main Org." {
+		t.Error("Not correctly parsing returned organizations.")
+	}
+}
+
+func TestOrgByName(t *testing.T) {
+	server, client := gapiTestTools(200, getOrgJSON)
+	defer server.Close()
+
+	org := "Main Org."
+	resp, err := client.OrgByName(org)
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Log(pretty.PrettyFormat(resp))
+
+	if resp.Id != 1 || resp.Name != org {
+		t.Error("Not correctly parsing returned organization.")
+	}
+}
+
+func TestOrg(t *testing.T) {
+	server, client := gapiTestTools(200, getOrgJSON)
+	defer server.Close()
+
+	org := int64(1)
+	resp, err := client.Org(org)
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Log(pretty.PrettyFormat(resp))
+
+	if resp.Id != org || resp.Name != "Main Org." {
+		t.Error("Not correctly parsing returned organization.")
+	}
+}
+
+func TestNewOrg(t *testing.T) {
+	server, client := gapiTestTools(200, createdOrgJSON)
+	defer server.Close()
+
+	resp, err := client.NewOrg("test-org")
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Log(pretty.PrettyFormat(resp))
+
+	if resp.OrgId != 1 {
+		t.Error("Not correctly parsing returned creation message.")
+	}
+}
+
+func TestUpdateOrg(t *testing.T) {
+	server, client := gapiTestTools(200, updatedOrgJSON)
+	defer server.Close()
+
+	err := client.UpdateOrg(int64(1), "test-org")
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestDeleteOrg(t *testing.T) {
+	server, client := gapiTestTools(200, deletedOrgJSON)
+	defer server.Close()
+
+	err := client.DeleteOrg(int64(1))
+	if err != nil {
+		t.Error(err)
+	}
+}
