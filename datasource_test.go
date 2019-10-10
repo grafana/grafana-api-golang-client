75c91ff3e47c60708ea78043987177f9c830516c
Holden Omans <holden.omans@gmail.com>
2019-10-09 20:57:42 -0400

    Added datasource prometheus values for json data (#23)
    * add missing fields for datasources
    
    * remove EsVersion
    
    * added test and corrected json syntax
    
    * Update datasource_test.go

--- datasource_test.go
+++ datasource_test.go
@@ -73,3 +73,32 @@ func TestNewDataSource(t *testing.T) {
 		t.Error("datasource creation response should return the created datasource ID")
 	}
 }
+
+func TestNewPrometheusDataSource(t *testing.T) {
+	server, client := gapiTestTools(200, createdDataSourceJSON)
+	defer server.Close()
+
+	ds := &DataSource{
+		Name:      "foo_prometheus",
+		Type:      "prometheus",
+		URL:       "http://some-url.com",
+		Access:    "access",
+		IsDefault: true,
+		JSONData: JSONData{
+			HttpMethod:   "POST",
+			QueryTimeout: "60s",
+			TimeInterval: "1m",
+		},
+	}
+
+	created, err := client.NewDataSource(ds)
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Log(pretty.PrettyFormat(created))
+
+	if created != 1 {
+		t.Error("datasource creation response should return the created datasource ID")
+	}
+}
