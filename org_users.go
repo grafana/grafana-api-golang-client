abaca28d4a1a2a7d74f33ded4780da20666a3f7d
Mike Ball <mikedball@gmail.com>
2020-06-06 08:07:16 -0400

    org users methods use central request method

--- org_users.go
+++ org_users.go
@@ -3,7 +3,6 @@ package gapi
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io/ioutil"
 )
@@ -16,19 +15,14 @@ type OrgUser struct {
 	Role   string `json:"role"`
 }
 
+// OrgUsers fetches and returns the users for the org whose ID it's passed.
 func (c *Client) OrgUsers(orgId int64) ([]OrgUser, error) {
 	users := make([]OrgUser, 0)
-	req, err := c.newRequest("GET", fmt.Sprintf("/api/orgs/%d/users", orgId), nil, nil)
+	resp, err := c.request("GET", fmt.Sprintf("/api/orgs/%d/users", orgId), nil, nil)
 	if err != nil {
 		return users, err
 	}
-	resp, err := c.Do(req)
-	if err != nil {
-		return users, err
-	}
-	if resp.StatusCode != 200 {
-		return users, errors.New(resp.Status)
-	}
+
 	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return users, err
@@ -40,6 +34,7 @@ func (c *Client) OrgUsers(orgId int64) ([]OrgUser, error) {
 	return users, err
 }
 
+// AddOrgUser adds a user to an org with the specified role.
 func (c *Client) AddOrgUser(orgId int64, user, role string) error {
 	dataMap := map[string]string{
 		"loginOrEmail": user,
@@ -49,20 +44,13 @@ func (c *Client) AddOrgUser(orgId int64, user, role string) error {
 	if err != nil {
 		return err
 	}
-	req, err := c.newRequest("POST", fmt.Sprintf("/api/orgs/%d/users", orgId), nil, bytes.NewBuffer(data))
-	if err != nil {
-		return err
-	}
-	resp, err := c.Do(req)
-	if err != nil {
-		return err
-	}
-	if resp.StatusCode != 200 {
-		return errors.New(resp.Status)
-	}
+
+	_, err = c.request("POST", fmt.Sprintf("/api/orgs/%d/users", orgId), nil, bytes.NewBuffer(data))
+
 	return err
 }
 
+// UpdateOrgUser updates and org user.
 func (c *Client) UpdateOrgUser(orgId, userId int64, role string) error {
 	dataMap := map[string]string{
 		"role": role,
@@ -71,31 +59,14 @@ func (c *Client) UpdateOrgUser(orgId, userId int64, role string) error {
 	if err != nil {
 		return err
 	}
-	req, err := c.newRequest("PATCH", fmt.Sprintf("/api/orgs/%d/users/%d", orgId, userId), nil, bytes.NewBuffer(data))
-	if err != nil {
-		return err
-	}
-	resp, err := c.Do(req)
-	if err != nil {
-		return err
-	}
-	if resp.StatusCode != 200 {
-		return errors.New(resp.Status)
-	}
+	_, err = c.request("PATCH", fmt.Sprintf("/api/orgs/%d/users/%d", orgId, userId), nil, bytes.NewBuffer(data))
+
 	return err
 }
 
+// RemoveOrgUser removes a user from an org.
 func (c *Client) RemoveOrgUser(orgId, userId int64) error {
-	req, err := c.newRequest("DELETE", fmt.Sprintf("/api/orgs/%d/users/%d", orgId, userId), nil, nil)
-	if err != nil {
-		return err
-	}
-	resp, err := c.Do(req)
-	if err != nil {
-		return err
-	}
-	if resp.StatusCode != 200 {
-		return errors.New(resp.Status)
-	}
+	_, err := c.request("DELETE", fmt.Sprintf("/api/orgs/%d/users/%d", orgId, userId), nil, nil)
+
 	return err
 }
