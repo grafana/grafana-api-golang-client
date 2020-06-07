aec31bc428f3fc7dfa17ac209d4836aadc36d2bc
Mike Ball <mikedball@gmail.com>
2020-06-07 18:38:01 -0400

    add support for alerting API
    This addresses issue #54.
    
    It should be noted that it builds on top of PR #51, which should probably
    be reviewed and, ideally, merged first, assuming PR #51 is approved.

--- admin_test.go
+++ admin_test.go
@@ -1,12 +1,21 @@
 package gapi
 
 import (
+	"strings"
 	"testing"
+
+	"github.com/gobs/pretty"
 )
 
 const (
 	createUserJSON = `{"id":1,"message":"User created"}`
 	deleteUserJSON = `{"message":"User deleted"}`
+
+	pauseAllAlertsJSON = `{
+		"alertsAffected": 1,
+		"state": "Paused",
+		"message": "alert paused"
+	}`
 )
 
 func TestCreateUser(t *testing.T) {
@@ -35,5 +44,31 @@ func TestDeleteUser(t *testing.T) {
 	err := client.DeleteUser(int64(1))
 	if err != nil {
 		t.Error(err)
+	}
+}
+
+func TestPauseAllAlerts(t *testing.T) {
+	server, client := gapiTestTools(200, pauseAllAlertsJSON)
+	defer server.Close()
+
+	res, err := client.PauseAllAlerts()
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Log(pretty.PrettyFormat(res))
+
+	if res.State != "Paused" {
+		t.Error("pause all alerts response should contain the correct response message")
+	}
+}
+
+func TestPauseAllAlerts_500(t *testing.T) {
+	server, client := gapiTestTools(500, pauseAllAlertsJSON)
+	defer server.Close()
+
+	_, err := client.PauseAllAlerts()
+	if !strings.Contains(err.Error(), "status: 500") {
+		t.Errorf("expected error to contain 'status: 500'; got: %s", err.Error())
 	}
 }
