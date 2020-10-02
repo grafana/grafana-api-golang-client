d576df4050633afbc3fa301429096847adff16b4
jwierzbo <jakub.wierzbowski@barco.com>
2020-10-02 17:23:58 +0200

    Add DataSourceGeneric resource

---
+++ datasource_generic.go
@@ -0,0 +1,130 @@
+package gapi
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+)
+
+type DataSourceGeneric struct {
+	Id     int64  `json:"id,omitempty"`
+	Name   string `json:"name"`
+	Type   string `json:"type"`
+	URL    string `json:"url"`
+	Access string `json:"access"`
+
+	Database string `json:"database,omitempty"`
+	User     string `json:"user,omitempty"`
+	// Deprecated in favor of secureJsonData.password
+	Password string `json:"password,omitempty"`
+
+	OrgId     int64 `json:"orgId,omitempty"`
+	IsDefault bool  `json:"isDefault"`
+
+	BasicAuth     bool   `json:"basicAuth"`
+	BasicAuthUser string `json:"basicAuthUser,omitempty"`
+	// Deprecated in favor of secureJsonData.basicAuthPassword
+	BasicAuthPassword string `json:"basicAuthPassword,omitempty"`
+
+	JSONData       JsonData `json:"jsonData,omitempty"`
+	SecureJSONData JsonData `json:"secureJsonData,omitempty"`
+}
+
+type JsonData map[string]interface{}
+
+func (c *Client) NewDataSourceGeneric(s *DataSourceGeneric) (int64, error) {
+	data, err := json.Marshal(s)
+	if err != nil {
+		return 0, err
+	}
+	req, err := c.newRequest("POST", "/api/datasources", nil, bytes.NewBuffer(data))
+	if err != nil {
+		return 0, err
+	}
+
+	resp, err := c.Do(req)
+	if err != nil {
+		return 0, err
+	}
+	if resp.StatusCode != 200 {
+		return 0, errors.New(resp.Status)
+	}
+
+	data, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return 0, err
+	}
+
+	result := struct {
+		Id int64 `json:"id"`
+	}{}
+	err = json.Unmarshal(data, &result)
+	return result.Id, err
+}
+
+func (c *Client) UpdateDataSourceGeneric(s *DataSourceGeneric) error {
+	path := fmt.Sprintf("/api/datasources/%d", s.Id)
+	data, err := json.Marshal(s)
+	if err != nil {
+		return err
+	}
+	req, err := c.newRequest("PUT", path, nil, bytes.NewBuffer(data))
+	if err != nil {
+		return err
+	}
+
+	resp, err := c.Do(req)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != 200 {
+		return errors.New(resp.Status)
+	}
+
+	return nil
+}
+
+func (c *Client) DataSourceGeneric(id int64) (*DataSourceGeneric, error) {
+	path := fmt.Sprintf("/api/datasources/%d", id)
+	req, err := c.newRequest("GET", path, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != 200 {
+		return nil, errors.New(resp.Status)
+	}
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	result := &DataSourceGeneric{}
+	err = json.Unmarshal(data, &result)
+	return result, err
+}
+
+func (c *Client) DeleteDataSourceGeneric(id int64) error {
+	path := fmt.Sprintf("/api/datasources/%d", id)
+	req, err := c.newRequest("DELETE", path, nil, nil)
+	if err != nil {
+		return err
+	}
+
+	resp, err := c.Do(req)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != 200 {
+		return errors.New(resp.Status)
+	}
+
+	return nil
+}
---
+++ datasource_generic_test.go
@@ -0,0 +1,71 @@
+package gapi
+
+import (
+	"testing"
+
+	"github.com/gobs/pretty"
+)
+
+func TestNewDataSourceGeneric(t *testing.T) {
+	server, client := gapiTestTools(200, createdDataSourceJSON)
+	defer server.Close()
+
+	ds := &DataSourceGeneric{
+		Name:      "foo",
+		Type:      "cloudwatch",
+		URL:       "http://some-url.com",
+		Access:    "access",
+		IsDefault: true,
+		JSONData: JsonData{
+			"AssumeRoleArn":           "arn:aws:iam::123:role/some-role",
+			"AuthType":                "keys",
+			"CustomMetricsNamespaces": "SomeNamespace",
+			"DefaultRegion":           "us-east-1",
+			"TlsSkipVerify":           true,
+		},
+		SecureJSONData: JsonData{
+			"AccessKey": "123",
+			"SecretKey": "456",
+		},
+	}
+
+	created, err := client.NewDataSourceGeneric(ds)
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Log(pretty.PrettyFormat(created))
+
+	if created != 1 {
+		t.Error("datasource creation response should return the created datasource ID")
+	}
+}
+
+func TestNewPrometheusDataSourceGeneric(t *testing.T) {
+	server, client := gapiTestTools(200, createdDataSourceJSON)
+	defer server.Close()
+
+	ds := &DataSourceGeneric{
+		Name:      "foo_prometheus",
+		Type:      "prometheus",
+		URL:       "http://some-url.com",
+		Access:    "access",
+		IsDefault: true,
+		JSONData: JsonData{
+			"HttpMethod":   "POST",
+			"QueryTimeout": "60s",
+			"TimeInterval": "1m",
+		},
+	}
+
+	created, err := client.NewDataSourceGeneric(ds)
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Log(pretty.PrettyFormat(created))
+
+	if created != 1 {
+		t.Error("datasource creation response should return the created datasource ID")
+	}
+}
