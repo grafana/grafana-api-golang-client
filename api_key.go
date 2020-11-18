0808370d3f0657fb071deb5281ee01e5086568f5
Edgar Orendain <edgar@orendainx.com>
2020-11-18 09:54:17 -0800

    LINTING: Uppercase certain acronyms.

--- api_key.go
+++ api_key.go
@@ -9,33 +9,33 @@ import (
 	"time"
 )
 
-type CreateApiKeyRequest struct {
+type CreateAPIKeyRequest struct {
 	Name          string `json:"name"`
 	Role          string `json:"role"`
 	SecondsToLive int64  `json:"secondsToLive,omitempty"`
 }
 
-type CreateApiKeyResponse struct {
-	// Id field only returned after Grafana v7.
-	Id   int64  `json:"id,omitempty"`
+type CreateAPIKeyResponse struct {
+	// ID field only returned after Grafana v7.
+	ID   int64  `json:"id,omitempty"`
 	Name string `json:"name"`
 	Key  string `json:"key"`
 }
 
-type GetApiKeysResponse struct {
-	Id         int64     `json:"id"`
+type GetAPIKeysResponse struct {
+	ID         int64     `json:"id"`
 	Name       string    `json:"name"`
 	Role       string    `json:"role"`
 	Expiration time.Time `json:"expiration,omitempty"`
 }
 
-type DeleteApiKeyResponse struct {
+type DeleteAPIKeyResponse struct {
 	Message string `json:"message"`
 }
 
-// CreateApiKey creates a new Grafana API key.
-func (c *Client) CreateApiKey(request CreateApiKeyRequest) (CreateApiKeyResponse, error) {
-	response := CreateApiKeyResponse{}
+// CreateAPIKey creates a new Grafana API key.
+func (c *Client) CreateAPIKey(request CreateAPIKeyRequest) (CreateAPIKeyResponse, error) {
+	response := CreateAPIKeyResponse{}
 
 	data, err := json.Marshal(request)
 	if err != nil {
@@ -46,9 +46,9 @@ func (c *Client) CreateApiKey(request CreateApiKeyRequest) (CreateApiKeyResponse
 	return response, err
 }
 
-// GetApiKeys retrieves a list of all API keys.
-func (c *Client) GetApiKeys(includeExpired bool) ([]*GetApiKeysResponse, error) {
-	response := make([]*GetApiKeysResponse, 0)
+// GetAPIKeys retrieves a list of all API keys.
+func (c *Client) GetAPIKeys(includeExpired bool) ([]*GetAPIKeysResponse, error) {
+	response := make([]*GetAPIKeysResponse, 0)
 
 	query := url.Values{}
 	query.Add("includeExpired", strconv.FormatBool(includeExpired))
@@ -57,9 +57,9 @@ func (c *Client) GetApiKeys(includeExpired bool) ([]*GetApiKeysResponse, error) 
 	return response, err
 }
 
-// DeleteApiKey deletes the Grafana API key with the specified ID.
-func (c *Client) DeleteApiKey(id int64) (DeleteApiKeyResponse, error) {
-	response := DeleteApiKeyResponse{}
+// DeleteAPIKey deletes the Grafana API key with the specified ID.
+func (c *Client) DeleteAPIKey(id int64) (DeleteAPIKeyResponse, error) {
+	response := DeleteAPIKeyResponse{}
 
 	path := fmt.Sprintf("/api/auth/keys/%d", id)
 	err := c.request("DELETE", path, nil, nil, &response)
