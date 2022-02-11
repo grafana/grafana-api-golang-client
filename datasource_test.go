d1646a93e04633243dae3d07cbf9293bdb236eba
steven leadbeater <stevenleadbeater@live.co.uk>
2022-02-11 10:31:11 +0000

    Use better example of token in tests

--- datasource_test.go
+++ datasource_test.go
@@ -155,7 +155,7 @@ func TestNewInfluxDBDataSource(t *testing.T) {
 			Version:         "Flux",
 		},
 		SecureJSONData: SecureJSONData{
-			httpHeaderValues: []string{"Bearer eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY0NDUwNjM3MywiaWF0IjoxNjQ0NTA2MzczfQ.FyYlVnMgzcP3CoNCHf2GFW49Ng_wLQsrXrUdSNGiShU"},
+			httpHeaderValues: []string{"Token alksdjaslkdjkslajdkj.asdlkjaksdjlkajsdlkjsaldj=="},
 		},
 	}
 
