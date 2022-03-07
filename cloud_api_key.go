154291db2e3230a59a13526516e0003dd742b9a1
Julien Duchesne <julien.duchesne@grafana.com>
2022-03-07 08:25:04 -0500

    Add function to create a temporary client

--- cloud_api_key.go
+++ cloud_api_key.go
@@ -23,21 +23,6 @@ type CloudAPIKey struct {
 	Expiration string
 }
 
-// This function creates a API key inside the Grafana instance running in stack `stack`. It's used in order
-// to provision API keys inside Grafana while just having access to a Grafana Cloud API key.
-//
-// See https://grafana.com/docs/grafana-cloud/api/#create-grafana-api-keys for more information.
-func (c *Client) CreateGrafanaAPIKeyFromCloud(stack string, input *CreateAPIKeyRequest) (*CreateAPIKeyResponse, error) {
-	resp := CreateAPIKeyResponse{}
-	data, err := json.Marshal(input)
-	if err != nil {
-		return nil, err
-	}
-
-	err = c.request("POST", fmt.Sprintf("/api/instances/%s/api/auth/keys", stack), nil, bytes.NewBuffer(data), &resp)
-	return &resp, err
-}
-
 func (c *Client) CreateCloudAPIKey(org string, input *CreateCloudAPIKeyInput) (*CloudAPIKey, error) {
 	resp := CloudAPIKey{}
 	data, err := json.Marshal(input)
