41a806bc876bf1088546dd03da26ffaba8b8768b
Matt Nolf <matt.nolf@grafana.com>
2022-03-08 11:53:28 +0000

    Add functionality to manage plugins

---
+++ plugin.go
@@ -0,0 +1,98 @@
+package gapi
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type Plugin struct {
+	Name        string `json:"name"`
+	Slug        string `json:"slug"`
+	Version     string `json:"version"`
+	Description string `json:"description"`
+}
+
+// InstallPlugin installs the specified plugin to the given stack.
+func (c *Client) InstallPlugin(stackID int64, pluginSlug string, pluginVersion string) error {
+	installPluginRequest := struct {
+		Plugin  string `json:"plugin"`
+		Version string `json:"version"`
+	}{
+		Plugin:  pluginSlug,
+		Version: pluginVersion,
+	}
+
+	data, err := json.Marshal(installPluginRequest)
+	if err != nil {
+		return err
+	}
+
+	err = c.request("POST", fmt.Sprintf("/api/instances/%d/plugins", stackID), nil, bytes.NewBuffer(data), nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// UninstallPlugin uninstalls the specified plugin to the given stack.
+func (c *Client) UninstallPlugin(stackID int64, pluginSlug string) error {
+	return c.request("DElETE", fmt.Sprintf("/api/instances/%d/plugins/%s", stackID, pluginSlug), nil, nil, nil)
+}
+
+func (c *Client) IsPluginInstalled(stackID int64, pluginSlug string) (bool, error) {
+	req, err := c.newRequest("GET", fmt.Sprintf("/api/instances/%d/plugins/%s", stackID, pluginSlug), nil, nil)
+	if err != nil {
+		return false, err
+	}
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return false, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		if resp.StatusCode == http.StatusNotFound {
+			return false, nil
+		}
+		bodyContents, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return false, err
+		}
+
+		return false, fmt.Errorf("status: %d, body: %v", resp.StatusCode, string(bodyContents))
+	}
+
+	return true, nil
+}
+
+// PluginBySlug returns the plugin with the given slug.
+// An error will be returned given an unknown slug.
+func (c *Client) PluginBySlug(slug string) (*Plugin, error) {
+	p := Plugin{}
+
+	err := c.request("GET", fmt.Sprintf("/api/plugins/%s", slug), nil, nil, &p)
+	if err != nil {
+		return nil, err
+	}
+
+	return &p, nil
+}
+
+// PluginByID returns the plugin with the given id.
+// An error will be returned given an unknown ID.
+func (c *Client) PluginByID(pluginID int64) (*Plugin, error) {
+	p := Plugin{}
+
+	err := c.request("GET", fmt.Sprintf("/api/plugins/%d", pluginID), nil, nil, p)
+	if err != nil {
+		return nil, err
+	}
+
+	return &p, nil
+}
---
+++ plugin_test.go
@@ -0,0 +1,177 @@
+package gapi
+
+import (
+	"testing"
+)
+
+const (
+	installPluginID   = 123
+	installPluginJSON = `
+	{
+		"id": 123,
+		"instanceId": 2,
+		"instanceUrl": "mystack.grafana.net",
+		"instanceSlug": "mystack",
+		"pluginId": 3,
+		"pluginSlug": "some-plugin",
+		"pluginName": "Some Plugin",
+		"version": "1.2.3",
+		"latestVersion": "1.2.3",
+		"createdAt": "2021-12-22T14:02:46.000Z",
+		"updatedAt": null
+	}`
+	uninstallPluginJSON = `
+	{
+		"id": 123,
+		"instanceId": 2,
+		"instanceUrl": "mystack.grafana.net",
+		"instanceSlug": "mystack",
+		"pluginId": 3,
+		"pluginSlug": "some-plugin",
+		"pluginName": "Some Plugin",
+		"version": "1.2.3",
+		"latestVersion": "1.2.3",
+		"createdAt": "2021-12-22T14:02:46.000Z",
+		"updatedAt": null
+	}`
+	getPluginJSON = `
+	{
+		"name": "Some Plugin",
+		"slug": "some-plugin",
+		"version": "1.2.3",
+		"description": "Some Plugin for adding functionality"
+	}`
+	notFoundJSON = `
+	{
+		"code": "NotFound",
+		"message": "instance plugin not found"
+	}`
+)
+
+func TestInstallPlugin(t *testing.T) {
+	server, client := gapiTestTools(t, 200, installPluginJSON)
+	defer server.Close()
+
+	err := client.InstallPlugin(int64(1), "some-plugin", "1.2.3")
+	if err != nil {
+		t.Error(err)
+	}
+
+	for _, code := range []int{401, 403, 404, 412} {
+		server.code = code
+
+		err = client.InstallPlugin(int64(1), "some-plugin", "1.2.3")
+		if err == nil {
+			t.Errorf("%d not detected", code)
+		}
+	}
+}
+
+func TestUninstallPlugin(t *testing.T) {
+	server, client := gapiTestTools(t, 200, uninstallPluginJSON)
+	defer server.Close()
+
+	err := client.UninstallPlugin(int64(1), "some-plugin")
+	if err != nil {
+		t.Error(err)
+	}
+
+	for _, code := range []int{401, 403, 404, 412} {
+		server.code = code
+
+		err = client.UninstallPlugin(int64(1), "some-plugin")
+		if err == nil {
+			t.Errorf("%d not detected", code)
+		}
+	}
+}
+
+func TestIsPluginInstalled(t *testing.T) {
+	server, client := gapiTestTools(t, 200, getPluginJSON)
+
+	ok, err := client.IsPluginInstalled(int64(1), "some-plugin")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !ok {
+		t.Errorf("Expected plugin installation - Expected true, got false")
+	}
+
+	server.code = 404
+	ok, err = client.IsPluginInstalled(int64(1), "some-plugin")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if ok {
+		t.Errorf("Unexpected plugin installation - Expected false, got true")
+	}
+
+	for _, code := range []int{401, 403, 412} {
+		server.code = code
+
+		_, err := client.IsPluginInstalled(int64(1), "some-plugin")
+		if err == nil {
+			t.Errorf("%d not detected", code)
+		}
+	}
+}
+
+func TestPlugin(t *testing.T) {
+	server, client := gapiTestTools(t, 200, getPluginJSON)
+	defer server.Close()
+
+	plugin, err := client.PluginBySlug("some-plugin")
+	if err != nil {
+		t.Error(err)
+	}
+
+	expectedPlugin := Plugin{}
+	err = UnmarshalJSONToStruct(getPluginJSON, &expectedPlugin)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if *plugin != expectedPlugin {
+		t.Errorf("Unexpected plugin - Actual: %v, Expected: %v", plugin, expectedPlugin)
+	}
+
+	for _, code := range []int{404} {
+		server.code = code
+
+		_, err = client.PluginBySlug("some-plugin")
+		if err == nil {
+			t.Errorf("%d not detected", code)
+		}
+	}
+}
+
+func TestPluginByID(t *testing.T) {
+	server, client := gapiTestTools(t, 200, getPluginJSON)
+	defer server.Close()
+
+	plugin, err := client.PluginBySlug("some-plugin")
+	if err != nil {
+		t.Error(err)
+	}
+
+	expectedPlugin := Plugin{}
+	err = UnmarshalJSONToStruct(getPluginJSON, &expectedPlugin)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if *plugin != expectedPlugin {
+		t.Errorf("Unexpected plugin - Actual: %v, Expected: %v", plugin, expectedPlugin)
+	}
+
+	for _, code := range []int{404} {
+		server.code = code
+
+		_, err = client.PluginByID(123)
+		if err == nil {
+			t.Errorf("%d not detected", code)
+		}
+	}
+}
