075eeb6a187ca5d455ec5e6329fb6a62a650eb50
MattNolf <86960799+matthewnolf@users.noreply.github.com>
2022-03-10 18:38:24 +0000

    Add functionality to manage plugins (#68)
    * Add functionality to manage plugins
    
    * Prefix plugin management with cloud
    
    * Use stack slug instead of ID
    
    * return ID of the installation
    
    * Return CloudInstallation

---
+++ cloud_plugin_test.go
@@ -0,0 +1,217 @@
+package gapi
+
+import (
+	"testing"
+)
+
+const (
+	installPluginJSON = `
+	{
+		"id": 123,
+		"instanceId": 2,
+		"instanceUrl": "mystack.grafana.net",
+		"instanceSlug": "mystack",
+		"pluginId": 3,
+		"pluginSlug": "some-plugin",
+		"pluginName": "Some Plugin",
+		"version": "1.2.3",
+		"latestVersion": "1.2.3",
+		"createdAt": "2021-12-22T14:02:46.000Z",
+		"updatedAt": null
+	}`
+	uninstallPluginJSON = `
+	{
+		"id": 123,
+		"instanceId": 2,
+		"instanceUrl": "mystack.grafana.net",
+		"instanceSlug": "mystack",
+		"pluginId": 3,
+		"pluginSlug": "some-plugin",
+		"pluginName": "Some Plugin",
+		"version": "1.2.3",
+		"latestVersion": "1.2.3",
+		"createdAt": "2021-12-22T14:02:46.000Z",
+		"updatedAt": null
+	}`
+	getPluginJSON = `
+	{
+		"id": 34,
+		"name": "Some Plugin",
+		"slug": "some-plugin",
+		"version": "1.2.3",
+		"description": "Some Plugin for adding functionality"
+	}`
+)
+
+func TestInstallCloudPlugin(t *testing.T) {
+	server, client := gapiTestTools(t, 200, installPluginJSON)
+	defer server.Close()
+
+	installation, err := client.InstallCloudPlugin("some-stack", "some-plugin", "1.2.3")
+	if err != nil {
+		t.Error(err)
+	}
+
+	expectedInstallation := CloudPluginInstallation{}
+	err = UnmarshalJSONToStruct(installPluginJSON, &expectedInstallation)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if *installation != expectedInstallation {
+		t.Errorf("Unexpected installation - Actual: %v, Expected: %v", installation, expectedInstallation)
+	}
+
+	for _, code := range []int{401, 403, 404, 412} {
+		server.code = code
+
+		installation, err = client.InstallCloudPlugin("some-stack", "some-plugin", "1.2.3")
+		if err == nil {
+			t.Errorf("%d not detected", code)
+		}
+		if installation != nil {
+			t.Errorf("Expected empty installation, got %v", installation)
+		}
+	}
+}
+
+func TestUninstallCloudPlugin(t *testing.T) {
+	server, client := gapiTestTools(t, 200, uninstallPluginJSON)
+	defer server.Close()
+
+	err := client.UninstallCloudPlugin("some-stack", "some-plugin")
+	if err != nil {
+		t.Error(err)
+	}
+
+	for _, code := range []int{401, 403, 404, 412} {
+		server.code = code
+
+		err = client.UninstallCloudPlugin("some-stack", "some-plugin")
+		if err == nil {
+			t.Errorf("%d not detected", code)
+		}
+	}
+}
+
+func TestIsCloudPluginInstalled(t *testing.T) {
+	server, client := gapiTestTools(t, 200, getPluginJSON)
+
+	ok, err := client.IsCloudPluginInstalled("some-stack", "some-plugin")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !ok {
+		t.Errorf("Expected plugin installation - Expected true, got false")
+	}
+
+	server.code = 404
+	ok, err = client.IsCloudPluginInstalled("some-stack", "some-plugin")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if ok {
+		t.Errorf("Unexpected plugin installation - Expected false, got true")
+	}
+
+	for _, code := range []int{401, 403, 412} {
+		server.code = code
+
+		_, err := client.IsCloudPluginInstalled("some-stack", "some-plugin")
+		if err == nil {
+			t.Errorf("%d not detected", code)
+		}
+	}
+}
+
+func TestGetCloudPluginInstallation(t *testing.T) {
+	server, client := gapiTestTools(t, 200, installPluginJSON)
+	defer server.Close()
+
+	installation, err := client.GetCloudPluginInstallation("some-stack", "some-plugin")
+	if err != nil {
+		t.Error(err)
+	}
+
+	expectedInstallation := CloudPluginInstallation{}
+	err = UnmarshalJSONToStruct(installPluginJSON, &expectedInstallation)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if *installation != expectedInstallation {
+		t.Errorf("Unexpected installation - Actual: %v, Expected: %v", installation, expectedInstallation)
+	}
+
+	for _, code := range []int{401, 403, 404, 412} {
+		server.code = code
+
+		installation, err = client.GetCloudPluginInstallation("some-stack", "some-plugin")
+		if err == nil {
+			t.Errorf("%d not detected", code)
+		}
+		if installation != nil {
+			t.Errorf("Expected empty installation, got %v", installation)
+		}
+	}
+}
+
+func TestPlugin(t *testing.T) {
+	server, client := gapiTestTools(t, 200, getPluginJSON)
+	defer server.Close()
+
+	plugin, err := client.PluginBySlug("some-plugin")
+	if err != nil {
+		t.Error(err)
+	}
+
+	expectedPlugin := Plugin{}
+	err = UnmarshalJSONToStruct(getPluginJSON, &expectedPlugin)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if *plugin != expectedPlugin {
+		t.Errorf("Unexpected plugin - Actual: %v, Expected: %v", plugin, expectedPlugin)
+	}
+
+	for _, code := range []int{404} {
+		server.code = code
+
+		_, err = client.PluginBySlug("some-plugin")
+		if err == nil {
+			t.Errorf("%d not detected", code)
+		}
+	}
+}
+
+func TestPluginByID(t *testing.T) {
+	server, client := gapiTestTools(t, 200, getPluginJSON)
+	defer server.Close()
+
+	plugin, err := client.PluginBySlug("some-plugin")
+	if err != nil {
+		t.Error(err)
+	}
+
+	expectedPlugin := Plugin{}
+	err = UnmarshalJSONToStruct(getPluginJSON, &expectedPlugin)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if *plugin != expectedPlugin {
+		t.Errorf("Unexpected plugin - Actual: %v, Expected: %v", plugin, expectedPlugin)
+	}
+
+	for _, code := range []int{404} {
+		server.code = code
+
+		_, err = client.PluginByID(123)
+		if err == nil {
+			t.Errorf("%d not detected", code)
+		}
+	}
+}
