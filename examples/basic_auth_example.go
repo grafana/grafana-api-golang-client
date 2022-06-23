5b650f9f57a1be2657ad8ec00396b621944f278e
Sofia Papagiannaki <papagian@gmail.com>
2022-06-23 19:22:48 +0300

    Add an example request with basic authentication

---
+++ examples/basic_auth_example.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+
+	gapi "github.com/grafana/grafana-api-golang-client"
+	"github.com/grafana/grafana-api-golang-client/goclient/client/datasources"
+	"github.com/grafana/grafana-api-golang-client/goclient/models"
+)
+
+func main() {
+	c, err := gapi.GetClient("http://localhost:3000")
+	if err != nil {
+		log.Fatalf("failed to get the client: %v", err)
+	}
+
+	params := datasources.NewAddDatasourceParams().WithBody(
+		&models.AddDataSourceCommand{
+			Name:      "foo",
+			Type:      "cloudwatch",
+			URL:       "http://some-url.com",
+			Access:    "access",
+			IsDefault: true,
+			JSONData: map[string]string{
+				"assumeRoleArn":           "arn:aws:iam::123:role/some-role",
+				"authType":                "keys",
+				"customMetricsNamespaces": "SomeNamespace",
+				"defaultRegion":           "us-east-1",
+				"tlsSkipVerify":           "true",
+			},
+			SecureJSONData: map[string]string{
+				"accessKey": "123",
+				"secretKey": "456",
+			},
+		},
+	)
+
+	res, err := c.Datasources.AddDatasource(params, gapi.BasicAuthenticator{
+		Username: "admin",
+		Password: "admin",
+	})
+	if err != nil {
+		log.Fatalf("request failed: %v", err)
+	}
+
+	log.Println("response", res.Payload)
+}
--- util.go
+++ util.go
@@ -1,10 +1,24 @@
 package gapi
 
 import (
+	"encoding/base64"
+	"fmt"
 	"net/url"
 
+	"github.com/go-openapi/runtime"
+	"github.com/go-openapi/strfmt"
 	"github.com/grafana/grafana-api-golang-client/goclient/client"
 )
+
+type BasicAuthenticator struct {
+	Username string
+	Password string
+}
+
+func (a BasicAuthenticator) AuthenticateRequest(req runtime.ClientRequest, reg strfmt.Registry) error {
+	creds := fmt.Sprintf("%s:%s", a.Username, a.Password)
+	return req.SetHeaderParam("Authorization", fmt.Sprintf("Basic %s", base64.StdEncoding.EncodeToString([]byte(creds))))
+}
 
 func GetClient(serverURL string) (*client.GrafanaHTTPAPI, error) {
 	u, err := url.Parse(serverURL)
