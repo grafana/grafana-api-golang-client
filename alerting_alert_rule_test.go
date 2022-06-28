61550ff997d03987c971acc2351a20f7101740bb
Steven Richards <steven.richards@ookla.com>
2022-06-27 17:00:21 -0700

    Add AlertRule type and methods
    Authored-by: Steven Richards <steven.richards@ookla.com>
    Co-authored-by: Alexander Weaver <weaver.alex.d@gmail.com>

---
+++ alerting_alert_rule_test.go
@@ -0,0 +1,130 @@
+package gapi
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/gobs/pretty"
+)
+
+func TestAlertRules(t *testing.T) {
+	t.Run("get alert rule succeeds", func(t *testing.T) {
+		server, client := gapiTestTools(t, 200, getAlertRuleJSON)
+		defer server.Close()
+
+		alertRule, err := client.AlertRule("123abcd")
+
+		if err != nil {
+			t.Error(err)
+		}
+		if alertRule.UID != "123abcd" {
+			t.Errorf("incorrect UID - expected %s got %#v", "123abcd", alertRule.UID)
+		}
+	})
+
+	t.Run("get non-existent alert rule fails", func(t *testing.T) {
+		server, client := gapiTestTools(t, 404, "")
+		defer server.Close()
+
+		alertRule, err := client.AlertRule("does not exist")
+
+		if err == nil {
+			t.Errorf("expected error but got nil")
+			t.Log(pretty.PrettyFormat(alertRule))
+		}
+	})
+
+	t.Run("create alert rule succeeds", func(t *testing.T) {
+		server, client := gapiTestTools(t, 201, writeAlertRuleJSON)
+		defer server.Close()
+		alertRule := createAlertRule()
+		uid, err := client.NewAlertRule(&alertRule)
+
+		if err != nil {
+			t.Error(err)
+		}
+		if uid != "123abcd" {
+			t.Errorf("unexpected UID returned, got %s", uid)
+		}
+	})
+
+	t.Run("update alert rule succeeds", func(t *testing.T) {
+		server, client := gapiTestTools(t, 200, writeAlertRuleJSON)
+		defer server.Close()
+		alertRule := createAlertRule()
+		alertRule.UID = "foobar"
+
+		err := client.UpdateAlertRule(&alertRule)
+
+		if err != nil {
+			t.Error(err)
+		}
+	})
+
+	t.Run("delete alert rule succeeds", func(t *testing.T) {
+		server, client := gapiTestTools(t, 204, "")
+		defer server.Close()
+
+		err := client.DeleteAlertRule("123abcd")
+
+		if err != nil {
+			t.Error(err)
+		}
+	})
+}
+
+func createAlertRule() AlertRule {
+	return AlertRule{
+		Condition:    "A",
+		Data:         createAlertQueries(),
+		ExecErrState: ErrOK,
+		FolderUID:    "project_test",
+		NoDataState:  NoDataOk,
+		OrgID:        1,
+		RuleGroup:    "eval_group_1",
+		Title:        "Always in alarm",
+		ForDuration:  0,
+	}
+}
+
+func createAlertQueries() []*AlertQuery {
+	alertQueries := make([]*AlertQuery, 1)
+	alertQueries[0] = &AlertQuery{
+		DatasourceUID:     "-100",
+		Model:             json.RawMessage(`{"datasourceUid":"-100","model":{"conditions":[{"evaluator":{"params":[0,0],"type":"gt"},"operator":{"type":"and"},"query":{"params":[]},"reducer":{"params":[],"type":"avg"},"type":"query"}],"datasource":{"type":"__expr__","uid":"__expr__"},"expression":"1 == 1","hide":false,"intervalMs":1000,"maxDataPoints":43200,"refId":"A","type":"math"},"queryType":"","refId":"A","relativeTimeRange":{"from":0,"to":0}}`),
+		QueryType:         "",
+		RefID:             "A",
+		RelativeTimeRange: RelativeTimeRange{From: 0, To: 0},
+	}
+	return alertQueries
+}
+
+const writeAlertRuleJSON = `
+	{
+	"conditions": "A",
+	"data": [{"datasourceUid":"-100","model":{"conditions":[{"evaluator":{"params":[0,0],"type":"gt"},"operator":{"type":"and"},"query":{"params":[]},"reducer":{"params":[],"type":"avg"},"type":"query"}],"datasource":{"type":"__expr__","uid":"__expr__"},"expression":"1 == 1","hide":false,"intervalMs":1000,"maxDataPoints":43200,"refId":"A","type":"math"},"queryType":"","refId":"A","relativeTimeRange":{"from":0,"to":0}}],
+	"uid": "123abcd",
+	"execErrState": "OK",
+	"folderUID": "project_test",
+	"noDataState": "OK",
+	"orgId": 1,
+	"ruleGroup": "eval_group_1",
+	"title": "Always in alarm",
+	"for": 0
+}
+`
+
+const getAlertRuleJSON = `
+	{
+	"conditions": "A",
+	"data": [{"datasourceUid":"-100","model":{"conditions":[{"evaluator":{"params":[0,0],"type":"gt"},"operator":{"type":"and"},"query":{"params":[]},"reducer":{"params":[],"type":"avg"},"type":"query"}],"datasource":{"type":"__expr__","uid":"__expr__"},"expression":"1 == 1","hide":false,"intervalMs":1000,"maxDataPoints":43200,"refId":"A","type":"math"},"queryType":"","refId":"A","relativeTimeRange":{"from":0,"to":0}}],
+	"execErrState": "OK",
+	"folderUID": "project_test",
+	"noDataState": "OK",
+	"orgId": 1,
+	"uid": "123abcd",
+	"ruleGroup": "eval_group_1",
+	"title": "Always in alarm",
+	"for": 0
+}
+`
