592b53249f8bb2505942b82361a9db6c8341259f
Vardan Torosyan <vardants@gmail.com>
2022-07-12 20:11:49 +0100

    Quick sketch of gapi for role assignments

---
+++ role_assignments.go
@@ -0,0 +1,23 @@
+package gapi
+
+type RoleAssignments struct {
+	UID            string               `json:"uid"`
+	Users          []UserRoleAssignment `json:"users,omitempty"`
+	Teams          []int                `json:"teams,omitempty"`
+	ServiceAccount []int                `json:"serviceAccounts,omitempty"`
+}
+
+type UserRoleAssignment struct {
+	ID     int64 `json:"id,omitempty"`
+	Global bool  `json:"global"`
+}
+
+func (c *Client) GetRoleAssignments(uid string) (*[]RoleAssignments, error) {
+	// implement me
+	return &[]RoleAssignments{}, nil
+}
+
+func (c *Client) UpdateRoleAssignments(ra RoleAssignments) error {
+	// implement me
+	return nil
+}
