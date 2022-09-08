383ad61e579a45147958536115b9640bda22a0f0
IevaVasiljeva <ieva.vasiljeva@grafana.com>
2022-09-08 23:01:07 +0100

    API client for reading and setting role assignments

---
+++ role_assignments.go
@@ -0,0 +1,48 @@
+package gapi
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type RoleAssignments struct {
+	RoleUID         string           `json:"role_uid"`
+	Users           []RoleAssignment `json:"users,omitempty"`
+	Teams           []RoleAssignment `json:"teams,omitempty"`
+	ServiceAccounts []RoleAssignment `json:"service_accounts,omitempty"`
+}
+
+type RoleAssignment struct {
+	ID     int  `json:"id"`
+	Global bool `json:"global"`
+}
+
+func (c *Client) GetRoleAssignments(uid string) (*RoleAssignments, error) {
+	assignments := &RoleAssignments{}
+	url := fmt.Sprintf("/api/access-control/roles/%s/assignments", uid)
+	if err := c.request(http.MethodGet, url, nil, nil, assignments); err != nil {
+		return nil, err
+	}
+
+	return assignments, nil
+}
+
+func (c *Client) UpdateRoleAssignments(ra RoleAssignments) (*RoleAssignments, error) {
+	response := &RoleAssignments{}
+
+	data, err := json.Marshal(ra)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO check method
+	url := fmt.Sprintf("/api/access-control/roles/%s/assignments", ra.RoleUID)
+	err = c.request(http.MethodPut, url, nil, bytes.NewBuffer(data), &response)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
