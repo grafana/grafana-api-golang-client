a771628d28c8eea26565b04c9128b0e95555b0d9
Julien Duchesne <julien.duchesne@grafana.com>
2023-01-10 22:01:28 -0500

    Fix linting

--- client.go
+++ client.go
@@ -160,9 +160,10 @@ func (c *Client) newRequest(method, requestPath string, query url.Values, body i
 		return req, err
 	}
 
-	if c.config.APIKey != "" && c.config.OrgID != 0 {
-		return req, fmt.Errorf("cannot use both API key and org ID. API keys are scoped to single org")
-	} else if c.config.APIKey != "" {
+	if c.config.APIKey != "" {
+		if c.config.OrgID != 0 {
+			return req, fmt.Errorf("cannot use both API key and org ID. API keys are scoped to single org")
+		}
 		req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", c.config.APIKey))
 	} else if c.config.OrgID != 0 {
 		req.Header.Add("X-Grafana-Org-Id", strconv.FormatInt(c.config.OrgID, 10))
