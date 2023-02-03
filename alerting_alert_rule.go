b816f5ee327789842629529b4dc44c5ffc518bd1
George Robinson <george.robinson@grafana.com>
2023-02-03 09:48:20 +0000

    Fix field name for IsPaused

--- alerting_alert_rule.go
+++ alerting_alert_rule.go
@@ -25,7 +25,7 @@ type AlertRule struct {
 	For          string            `json:"for"`
 	ForDuration  time.Duration     `json:"-"`
 	Provenance   string            `json:"provenance"`
-	IsPaused     bool              `json:"is_paused"`
+	IsPaused     bool              `json:"isPaused"`
 }
 
 // RuleGroup represents a group of rules in Grafana Alerting.
